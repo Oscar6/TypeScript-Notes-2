19b204722487a7c7bfec6cb7dcb623e537def9f5
Oscar6 <oscar.m1024@gmail.com>
2019-08-02 19:09:24 -0500

    basic types and .ts file added

---
+++ src/test.ts
@@ -0,0 +1,13 @@
+// Basic Types
+
+let name: array = ['Olive', 'Finn', 2, true];
+
+let person: string = 'Steve';
+
+let heightInCentimeters: number = 182.121212;
+
+let names: string[] = ['Olive', 'Finn', 'C3P0', '6', 'Engineer'];
+
+let numtest: number[] = [6, 12, 6, 8];
+
+let booltest: boolean[] = [true, false, false];
