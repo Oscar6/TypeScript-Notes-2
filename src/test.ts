fbdfd7d4c5b9608ec7efbdcd8c9a0e1032b97f18
Oscar6 <oscar.m1024@gmail.com>
2019-08-05 11:19:10 -0500

    Advanced Types

--- src/test.ts
+++ src/test.ts
@@ -29,4 +29,48 @@ function log(val: any) {
 // log(DoorStats.Open);
 log(DoorStats[0]);
 
-// Advance Types
+// Advance Types;
+
+// Union Types
+var unionType = string | number;
+
+unionType = 1;
+
+unionType = 'Hello';
+
+// Type Guards
+
+// function addWithUnion(arg: string | number, arg2: string | number) {
+//     return arg1 + arg2;
+// }
+
+function addWithTypeGuard(arg1: string | number, arg2: string | number) : string | number {
+    if(typeof arg1 === "string") {
+        console.log('First argument is a string');
+        return arg1 + arg2;
+    }
+
+    if(typeof arg1 === "number" && typeof arg2 === "number"){
+        console.log('both arguments are numbers');
+        return arg1 + arg2;
+    }
+
+    return arg1.toString() + arg2.toString();
+}
+
+// Type Aliases
+
+type StringOrNumber = string | number;
+
+function addWithAlias (arg1: StringOrNumber, arg2: StringOrNumber) {
+    return arg1.toString() + arg2.toString();
+}
+
+// Null and Undefined
+
+function testUndef(test: null | number) {
+    console.log('test parameter: ' + test);
+}
+
+testUndef(); // will output an error
+testUndef(test:null);
